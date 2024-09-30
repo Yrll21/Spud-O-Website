d1b3c68f04a4bac7890f0ddafeeedda4c03cbe9f
Ivan Yrll Labadia <yrll@Ivans-MacBook-Air.local>
2024-09-30 13:13:13 +0800

    Add button functionalities but only the UI parts

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,14 @@
 /*!
-* Start Bootstrap - Shop Homepage v5.0.6 (https://startbootstrap.com/template/shop-homepage)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-shop-homepage/blob/master/LICENSE)
-*/
+ * Start Bootstrap - Shop Homepage v5.0.6 (https://startbootstrap.com/template/shop-homepage)
+ * Copyright 2013-2023 Start Bootstrap
+ * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-shop-homepage/blob/master/LICENSE)
+ */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+let amount = document.getElementById("amount");
+function addAmount() {
+  amount.value++;
+}
+function reduceAmount() {
+  if (amount.value > 0) amount.value--;
+}
